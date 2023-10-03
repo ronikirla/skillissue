dc012686e4b42a18d657e0526623648dad091a3b
Roni Kirla <roni.kirla@gmail.com>
2023-10-04 02:51:08 +0300

    Shorten some lines

--- skillissue.py
+++ skillissue.py
@@ -10,15 +10,18 @@ def main():
                         help="LiveSplit splits file (.lss)")
     parser.add_argument("-w", "--weight",
                         type=restricted_float,
-                        help="Weight decay factor. The lower the values, the more your recent times are prioritized. Values between [0,1]. Default 15/16.",
+                        help="Weight decay factor. The lower the values, the more your recent times are prioritized. Values between [0,1].\
+                              Default 15/16.",
                         default=15/16)
     parser.add_argument("-m", "--min_weight",
                         type=restricted_float,
-                        help="Increase this to drop old attempts completely. After the weight of a time has decayed to this value, the time will be dropped. Useful with -H. Default 0.",
+                        help="Increase this to drop old attempts completely. After the weight of a time has decayed to this value, the time\
+                              will be dropped. Useful with -H. Default 0.",
                         default=0)
     parser.add_argument("-d", "--drop_missing",
                         action="store_true",
-                        help="By default, resets are counted as infinite time runs, essentially treating them as forfeits. This option ignores them instead.")
+                        help="By default, resets are counted as infinite time runs, essentially treating them as forfeits.\
+                              This option ignores them instead.")
     parser.add_argument("-s", "--start",
                         type=positive_int,
                         help="Older attempts than this will not be shown in the resulting graph. Default 1.",
@@ -32,14 +35,16 @@ def main():
                         help="Uses average instead of percentile for calculations. Forces -d and ignores -p.")
     parser.add_argument("-H", "--hist", "--histogram",
                         action="store_true",
-                        help="Output a histogram visualizing the distribution of your times instead of a progression chart. Forces -d and ignores -p and -a.")
+                        help="Output a histogram visualizing the distribution of your times instead of a progression chart.\
+                              Forces -d and ignores -p and -a.")
     parser.add_argument("-r", "--remake_window",
                         type=positive_int,
                         help="Number of seconds during which resets at the start of a run are not counted as a forfeit. Default 0.",
                         default=0)
     parser.add_argument("-P", "--per_split",
                         action="store_true",
-                        help="Generate the output splitwise instead of based on the finish times. Stores the output plots in a folder.")
+                        help="Generate the output splitwise instead of based on the finish times. Stores the splitwise plots in ./skillissue_output\
+                              and shows an aggregate plot of all splits.")
     parser.add_argument("-n", "--n_sims",
                         type=positive_int,
                         help="Specify how many simulations to perform when generating an aggregate splitwise histogram. Default 1000.",
